d2e21e5b26181efae1e0c010ff52b33db17afca7
Janet Matsen <janetmatsen@gmail.com>
2016-10-09 10:11:59 -0700

    plot of predictions by label works

--- HW1/Q6_least_squares/analyze_ridge_results.py
+++ HW1/Q6_least_squares/analyze_ridge_results.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import seaborn as sns
+
+
 def analyze_results(ridge_obj, cutoff):
 
     truth = ridge_obj.y.toarray()[:,0]
@@ -31,3 +35,14 @@ def analyze_results(ridge_obj, cutoff):
 
     return {"call_counts": call_counts, "y_vals": y_vals,
             "loss_01": loss_01, "loss_01_norm": loss_01_norm}
+
+def plot_results(preds, labels):
+
+    to_compare = pd.DataFrame({"predictions": preds,
+                               "labels": labels})
+
+    print(to_compare.head())
+    print(to_compare.shape)
+
+    sns.violinplot(x='labels', y='predictions',
+                   data=to_compare)
