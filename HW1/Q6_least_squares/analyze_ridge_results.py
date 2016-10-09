a2d3fc1d5d1721a400b45f6e5a0508f3c42e7035
Janet Matsen <janetmatsen@gmail.com>
2016-10-09 13:34:57 -0700

    tidyier

--- HW1/Q6_least_squares/analyze_ridge_results.py
+++ HW1/Q6_least_squares/analyze_ridge_results.py
@@ -7,14 +7,14 @@ def analyze_results(ridge_obj, cutoff):
     truth = ridge_obj.y.toarray()[:,0]
 
     # categorize the info
-    y_preds_for_3s = ridge_obj.y_preds[truth ==1]
+    y_preds_for_2s = ridge_obj.y_preds[truth ==1]
     y_preds_for_other_numbers = ridge_obj.y_preds[truth == 0]
 
-    true_positives = y_preds_for_3s [y_preds_for_3s >= cutoff]
+    true_positives = y_preds_for_2s [y_preds_for_2s >= cutoff]
     true_negatives = \
         y_preds_for_other_numbers[y_preds_for_other_numbers <= cutoff]
 
-    false_positives = y_preds_for_3s [y_preds_for_3s < cutoff]
+    false_positives = y_preds_for_2s [y_preds_for_2s < cutoff]
     false_negatives = \
         y_preds_for_other_numbers[y_preds_for_other_numbers > cutoff]
 
@@ -35,14 +35,3 @@ def analyze_results(ridge_obj, cutoff):
 
     return {"call_counts": call_counts, "y_vals": y_vals,
             "loss_01": loss_01, "loss_01_norm": loss_01_norm}
-
-def plot_results(preds, labels):
-
-    to_compare = pd.DataFrame({"predictions": preds,
-                               "labels": labels})
-
-    print(to_compare.head())
-    print(to_compare.shape)
-
-    sns.violinplot(x='labels', y='predictions',
-                   data=to_compare)
