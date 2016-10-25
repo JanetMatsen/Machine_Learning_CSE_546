c61855a184b2bd6b2d27338ebe914df40264751d
Janet Matsen <janetmatsen@gmail.com>
2016-10-24 17:31:30 -0700

    added multi-class Ridge.  Note that equivalent solutions give different SSE
    See saved jupyter notebook for evidence using a nearly-identity toy matrix.

--- HW2/code/ridge_regression.py
+++ HW2/code/ridge_regression.py
@@ -4,6 +4,83 @@ import scipy.sparse as sp
 
 from classification_base import ClassificationBase
 
+class RidgeMulti(ClassificationBase):
+    """
+    Train multiple ridge models.
+    """
+    def __init__(self, X, y, lam, W=None):
+        super(RidgeMulti, self).__init__(X=X, y=y, W=W)
+        self.lam = lam
+        self.W = None # don't want to have W before solving!
+        self.threshold = None
+        self.matrix_work = None
+
+    def get_weights(self):
+        return self.W
+
+    def apply_weights(self):
+        return self.X.dot(self.W)
+
+    def optimize(self):
+        # When solving multiclass, (X^TX + lambdaI)-1X^T is shared
+        # solve it once and share it with all the regressors.
+        # find lambda*I_D + X^T*X
+        piece_to_invert = np.identity(self.d)*self.lam + self.X.T.dot(self.X)
+        assert piece_to_invert.shape == (self.d, self.d)
+
+        inverted_piece = np.linalg.inv(piece_to_invert)
+
+        self.matrix_work = inverted_piece.dot(self.X.T)
+        assert self.matrix_work.shape == (self.d, self.N)
+
+        # Train C classifiers.
+        self.W = self.matrix_work.dot(self.Y)
+        assert self.W.shape == (self.d, self.C)
+        return self.W
+
+    def predict(self):
+        if self.W is None:
+            self.optimize()
+
+        Yhat = self.apply_weights()
+        classes = np.argmax(Yhat, axis=1)
+        yhat = np.multiply(self.Y, Yhat)
+        # collapse it into an Nx1 array:
+        self.yhat = np.amax(yhat, axis=1)
+        return classes
+
+    def run(self):
+        self.predict()
+        self.results = pd.DataFrame(self.results_row())
+
+    def loss_01(self):
+        return self.pred_to_01_loss(self.predict())
+
+    def results_row(self):
+        """
+        Return a dictionary that can be put into a Pandas DataFrame.
+        """
+        results_row = super(RidgeMulti, self).results_row()
+
+        # append on Ridge regression-specific results
+        more_details = {
+            "lambda":[self.lam],
+            "SSE":[self.sse()],
+            "RMSE":[self.rmse()],
+            }
+        results_row.update(more_details)
+        return results_row
+
+    def sse(self):
+        # sse = RSS
+        error = self.apply_weights() - self.Y
+        error = np.multiply(error, self.Y)
+        error = np.amax(np.abs(error), axis=1)
+        return error.T.dot(error)
+
+    def rmse(self):
+        return(self.sse()/self.N)**0.5
+
 
 class RidgeBinary(ClassificationBase):
     """
@@ -22,6 +99,9 @@ class RidgeBinary(ClassificationBase):
 
     def get_weights(self):
         return self.w
+
+    def apply_weights(self):
+        return self.X.dot(self.w)
 
     def run(self):
 
@@ -55,8 +135,6 @@ class RidgeBinary(ClassificationBase):
         results_row = super(RidgeBinary, self).results_row()
 
         # append on logistic regression-specific results
-        sse = self.sse()
-
         more_details = {
             "lambda":[self.lam],
             "SSE":[self.sse()],
@@ -67,8 +145,8 @@ class RidgeBinary(ClassificationBase):
 
     def sse(self):
         # sse = RSS
-        error_v = self.X.dot(self.w) - self.y
-        return error_v.T.dot(error_v)
+        error = self.apply_weights() - self.y
+        return error.T.dot(error)
 
     def rmse(self):
         return(self.sse()/self.N)**0.5
