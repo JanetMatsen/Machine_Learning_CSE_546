bbc7da4c6f2af6cc590df8e67fea8a1349ef557c
Janet Matsen <janetmatsen@gmail.com>
2016-10-26 11:24:59 -0700

    returns arrays of training and test data, including for binary

--- HW2/code/mnist_helpers.py
+++ HW2/code/mnist_helpers.py
@@ -1,4 +1,27 @@
 import numpy as np
 
+from mnist import MNIST
+
+from classification_base import MNIST_PATH
+
 def prep_binary_classes(y, digit=2):
     return np.array([int(x==digit) for x in y])
+
+def mnist_training():
+    mndata = MNIST(MNIST_PATH)
+    train_ims, train_labels = mndata.load_training()
+    train_X = np.array(train_ims)
+    train_y = np.array(train_labels)
+    return train_X, train_y
+
+def mnist_testing():
+    mndata = MNIST(MNIST_PATH)
+    test_ims, test_labels = mndata.load_testing()
+    test_X = np.array(test_ims)
+    test_y = np.array(test_labels)
+    return test_X, test_y
+
+def mnist_training_binary(num):
+    X, y = mnist_training()
+    return X, np.array([int(yi==num) for yi in y])
+
